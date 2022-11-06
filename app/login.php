b472cf4e444332ae101fd260f92dbfa858c2afe0
ZofiaZinkowska <zinkowskazosia20@gmail.com>
2022-11-06 18:08:39 +0100

    Changing the appearance of the website

--- app/login.php
+++ app/login.php
@@ -16,19 +16,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikacja do zakładania kont w bazie danych</title>
+    <link rel="shortcut icon" href="pobrane.png" type="image/x-icon">
+    <link rel="stylesheet" href="index.css" />
+    <title>Aplikacja do zakładania kont na serwerze baz danych</title>
 </head>
 <body>
+    <div id="container">
+            <nav>
+                <button><a class="menu" href="index.php">Strona główna</a></button>
+                <button><a class="menu" href="login.php">Login</a></button>
+                <button><a class="menu" href="register.php">Rejestracja</a></button>
+            </nav>
+    </div>
     <div>
         <form id="login" method="POST" action="">
-            <div>
+            <article>
                 <h2>Zaloguj się</h2>
-                <div>
+                <div class = "inputs">
                     <input name="email" type="email" placeholder="Podaj email" />
                     <input name="password" type="password" placeholder="Podaj haslo" />
 
                     <button type="submit">Zaloguj się</button>
                 </div>
+            </article>
             </div>
         </form>
     </div>
--- app/register.php
+++ app/register.php
@@ -18,14 +18,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikacja do zakładania kont w bazie danych</title>
+    <link rel="shortcut icon" href="pobrane.png" type="image/x-icon">
+    <link rel="stylesheet" href="index.css" />
+    <title>Aplikacja do zakładania kont na serwerze baz danych</title>
 </head>
 <body>
+    <div id="container">
+            <nav>
+                <button><a class="menu" href="index.php">Strona główna</a></button>
+                <button><a class="menu" href="login.php">Login</a></button>
+                <button><a class="menu" href="register.php">Rejestracja</a></button>
+            </nav>
+    </div>
     <div>
         <form id="register" method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        <article>
             <div>
                 <h2>Rejestracja</h2>
-                <div>
+                </article>
+                <div class = "inputs">
                     <input 
                         name="username" 
                         type="text"
@@ -41,7 +52,7 @@
                         type="password" 
                         placeholder="Podaj hasło" 
                     />
-                    <input type="submit" />
+                    <button type="submit">Zarejestruj się</button>
                     <?php
                     
                         if ($_SERVER["REQUEST_METHOD"] == "POST") {
